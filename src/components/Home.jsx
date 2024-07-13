6de4d8d28236d6f430c7093c5e0821dfb2375135
ABHIJEET <abhijeetsahoomsd@gmail.com>
2024-07-13 20:47:42 +0530

    FEAT: created the reusable card component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import HomeLayout from '../layouts/HomeLayout'
+import Card from './Card'
+import { BsFillPencilFill } from 'react-icons/bs'
 
 const Home = () => {
   return (
     <div>
       <HomeLayout>
-        
+        {/* <Card>
+          <BsFillPencilFill className='inline mr-2' />
+        </Card>
+
+        <Card status={30} background='bg-yellow-300' borderColor='border-green-500 ' fontColor='text-black' dividerColor='bg-black'>
+          <BsFillPencilFill className='inline mr-2' />
+        </Card> */}
+
       </HomeLayout>
     </div>
   )
